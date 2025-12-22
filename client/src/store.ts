94acf88f07ecfd26868c2c96d827fd0691e862e6
Petros Asegid <pasegid@gmail.com>
2025-12-22 13:52:00 +0300

    docs(api): update swagger definition

--- client/src/store.ts
+++ client/src/store.ts
@@ -16,3 +16,5 @@ import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+// Update 2025-12-22 13:52:00: docs(api): update swagger definition
