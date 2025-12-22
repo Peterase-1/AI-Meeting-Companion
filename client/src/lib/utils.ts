a52c26ed8538583512d54bd50e280b3f293ffac9
Petros Asegid <pasegid@gmail.com>
2025-12-22 11:28:00 +0300

    perf(db): optimize user query performance

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Update 2025-12-22 11:28:00: perf(db): optimize user query performance
